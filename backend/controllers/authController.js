961858aebedc8619ca3ee5f13a7333a7112d00e5
Vaibhav Agarwal <91776618+va6265@users.noreply.github.com>
2023-08-15 11:00:51 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     // httpOnly: true,
-    domain: 'snip-box-app.vercel.app',
+    domain: '.vercel.app',
     sameSite: "None",
     secure: true,
   };
